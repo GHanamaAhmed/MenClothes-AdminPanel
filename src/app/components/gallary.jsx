7066a20b478e7209c9f0b4846c35ed86bb3ec275
Edward crowe <96554603+0asaca0rum0@users.noreply.github.com>
2023-08-01 17:55:24 +0100

    added promotion,crud opertion for product

--- src/app/components/gallary.jsx
+++ src/app/components/gallary.jsx
@@ -17,7 +17,7 @@ export default function Gallary  ({images}) {
 						<img
 							src={image}
 							alt="gallary"
-							className="w-[200px] h-[200px] min-w-[200px] object-fill rounded-lg shadow-lg hover:w-[225px] hover:h-[225px] hover:shadow-xl transition-all shadow-gray-600"
+							className="w-[200px] h-[200px] min-w-[200px] object-cover rounded-lg shadow-lg hover:w-[225px] hover:h-[225px] hover:shadow-xl transition-all shadow-gray-600"
 						/>
 						<input
 							type="file"
@@ -25,17 +25,15 @@ export default function Gallary  ({images}) {
 							id="file-input"
 							className="hidden"
 						/>
-						<div className="absolute top-2 right-2" key={index}>
-							<IconButton className="absolute top-2 right-2 rounded-lg z-50 bg-transparent border-0 m-2 text-black shadow-none hover:shadow-md hover:shadow-red-600/50">
-								<MinusIcon
-									className="h-5 w-5 text-red-600"
-									onClick={() => {
-										console.log("wow");
-									}}
-								/>
-							</IconButton>
-						</div>
 					</label>
+						<IconButton className=" -top-12 -right-1 rounded-lg z-50 bg-transparent  text-black shadow-md hover:shadow-lg hover:shadow-red-600/50" color="red">
+							<MinusIcon
+								className="h-5 w-5 text-red-600"
+								onClick={() => {
+									console.log("wow");
+								}}
+							/>
+						</IconButton>
 				</div>
 			))}
 			<label htmlFor="file-input" className="cursor-pointer rounded-lg  p-2">
