010e6ba765a0c958d3673403e9bc87bf8f819729
Edward crowe <96554603+0asaca0rum0@users.noreply.github.com>
2023-08-17 03:11:02 +0100

    dashboar prodcution

---
+++ ecosystem.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+	apps: [
+		{
+      
+			name: "fri7a-dashboard",
+      script: "npm start",
+      watch: true,
+      env: {
+        NODE_ENV: "production",
+      },
+		},
+	],
+
+	deploy: {
+		production: {
+			user: "root",
+			host: "88.198.200.124",
+			ref: "origin/main",
+			repo: "git@github.com:0asaca0rum0/fri7a-dashboard.git",
+			path: "/home/test01",
+			"post-deploy":
+				"source ~/.nvm/nvm.sh &&  npm install && pm2 reload ecosystem.config.js --env production",
+		},
+	},
+};
